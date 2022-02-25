b6f025c934359016cb95e2b9b854b7b94ab18317
Dmrgn <danielrmorgan11@gmail.com>
2022-02-24 22:29:30 -0500

    Correct order of execs

--- Site/javascript/about.js
+++ Site/javascript/about.js
@@ -6,14 +6,24 @@ async function attachExecs() {
     const execs = await (await fetch("https://tedclub.herokuapp.com/data/execs")).json();
     // load positions file
     const positions = await (await fetch("json/positions.json")).json();
+    // the order each position should appear
+    const posindexs = [];
+    for (const [key, value] of Object.entries(positions)) {
+        posindexs.push(key);
+    }
     // load card html
     const card = await (await fetch("html/card.html")).text()
+    // function to sort cards based on position
+    function comparePos(a, b) {
+        return posindexs.indexOf(a.position) - posindexs.indexOf(b.position);
+    }
     // keys to replace
     const keys = {
         __NAME:"name",
         __POSITION:"position",
         __DESCRIPTION:"description",
     };
+    let cards = [];
     // for each exec
     for (const exec in execs) {
         let tempCard = card;
@@ -26,15 +36,16 @@ async function attachExecs() {
         // replace id
         tempCard = tempCard.replaceAll("__ID", "BUTTON"+exec);
         // append card to end of cards list
-        container.innerHTML+=tempCard;
-        // attach event listener to show more button
-        // document.querySelector(`#btnBUTTON${exec}`).addEventListener('click', function(e) {
-        //     console.log(target);
-        //     e.target.classList.toggle('is-hidden');
-        // });
+        cards.push({position:execs[exec]["position"], card:tempCard});
     }
-    const cards = document.querySelectorAll(".exec-card");
-    cards[cards.length-1].style["margin-bottom"] = "1.5rem";
+    // sort cards by position
+    cards = cards.sort(comparePos);
+    // add cards to dom
+    for (x in cards) {
+        container.innerHTML+=cards[x].card;
+    }
+    const allcards = document.querySelectorAll(".exec-card");
+    allcards[allcards.length-1].style["margin-bottom"] = "1.5rem";
 }
 
 attachExecs();
